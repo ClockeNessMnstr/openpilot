aaa9eaf08c9c641153dfa080cc125c17f5af73da
ClockeNessMnstr <locke.dftc@gmail.com>
2021-07-03 01:44:02 -0400

    Forester 30 step

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -6,7 +6,12 @@ Ecu = car.CarParams.Ecu
 
 class CarControllerParams:
   def __init__(self, CP):
-    self.STEER_DELTA_UP = 50           # torque increase per refresh, 0.8s to max
+    self.STEER_STEP = 2                # how often we update the steer cmd
+    self.STEER_DELTA_UP = 30           # torque increase per refresh, 0.8s to max
+    self.STEER_DELTA_DOWN = 30         # torque decrease per refresh
+    self.STEER_DRIVER_ALLOWANCE = 60   # allowed driver torque before start limiting
+    self.STEER_DRIVER_MULTIPLIER = 10  # weight driver torque heavily
+    self.STEER_DRIVER_FACTOR = 1       # from dbc
     if CP.carFingerprint == CAR.IMPREZA_2020:
       self.STEER_MAX = 1439
     elif CP.carFingerprint == CAR.IMPREZA_3071:
@@ -14,11 +19,6 @@ class CarControllerParams:
       self.STEER_DELTA_UP = 70
     else:
       self.STEER_MAX = 2047
-    self.STEER_STEP = 2                # how often we update the steer cmd
-    self.STEER_DELTA_DOWN = 70         # torque decrease per refresh
-    self.STEER_DRIVER_ALLOWANCE = 60   # allowed driver torque before start limiting
-    self.STEER_DRIVER_MULTIPLIER = 10  # weight driver torque heavily
-    self.STEER_DRIVER_FACTOR = 1       # from dbc
 
 class CAR:
   ASCENT = "SUBARU ASCENT LIMITED 2019"
