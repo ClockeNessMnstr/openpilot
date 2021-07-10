311354f2c12d18d8e187359b250a7c7ddc274d22
ClockeNessMnstr <locke.dftc@gmail.com>
2021-07-10 11:00:48 -0400

    cleanup PID no u0

--- selfdrive/controls/lib/pid.py
+++ selfdrive/controls/lib/pid.py
@@ -19,7 +19,7 @@ class PIDController():
     self._k_d = k_d  # derivative gain
     self.k_f = k_f  # feedforward gain
 
-    self.error = FirstOrderFilter(0, 20, DT_CTRL, hz_mode=True)
+    self.error = FirstOrderFilter(0, 2, DT_CTRL, hz_mode=True)
 
     self.pos_limit = pos_limit
     self.neg_limit = neg_limit
@@ -58,7 +58,6 @@ class PIDController():
     self.saturated = self.saturated and (self.sat_count > self.sat_limit)
 
   def reset(self):
-    self.u0, self.u1, self.u2 = 0.0, 0.0, 0.0
     self.e0, self.e1, self.e2 = 0.0, 0.0, 0.0
     self.bf1, self.bf2 = 0.0, 0.0
 
@@ -85,26 +84,16 @@ class PIDController():
     Kp, Ki, Kd = self.k_p, self.k_i, self.k_d
     a0, a1, a2 = (1 + _N*_Ts), -(2 + _N*_Ts), 1
     
-    b0 = Kp*a0 + Ki*_Ts*a0 + Kd*_N
-    b1 = Kp*a1 - Ki*_Ts  - 2*Kd*_N
-    b2 = Kp    +             Kd*_N
-    
     self.ku1, self.ku2 = a1/a0, a2/a0
-    self.ke0, self.ke1, self.ke2 = b0/a0, b1/a0, b2/a0 
 
-    self.e2, self.e1 = self.e1, self.e0
-    self.u2, self.u1 = self.u1, self.u0
-    
+    self.e2, self.e1 = self.e1, self.e0    
     self.e0 = float(apply_deadzone(setpoint - measurement, deadzone))
     self.e0 = self.error.update(self.e0)
 
-    self.u0 =  self.ke0*self.e0 + self.ke1*self.e1 + self.ke2*self.e2 - self.ku1*self.u1 - self.ku2*self.u2
-
     #back-feed / back-calculate integrator anti-windup
-    self.bf2, self.bf1 = self.bf1, self.u1 + self.f - last_output
-    self.u0 -= (k_bf*_Ts*(a0*self.bf1 - self.bf2)/ a0)
+    self.bf2 = self.bf1
+    self.bf1 = self.p + self.i + self.d + self.f - last_output
 
-    #logging only
     self.p2, self.p1 = self.p1, self.p
     self.i2, self.i1 = self.i1, self.i
     self.d2, self.d1 = self.d1, self.d
@@ -112,14 +101,13 @@ class PIDController():
     self.p = (Kp*(    a0*self.e0 + a1*self.e1 + self.e2) / a0) -self.ku1*self.p1 - self.ku2*self.p2
     self.i = (Ki*_Ts*(a0*self.e0 -    self.e1)           / a0) -self.ku1*self.i1 - self.ku2*self.i2 - (k_bf*_Ts*(a0*self.bf1 - self.bf2)/ a0)
     self.d = (Kd*_N*(    self.e0 -  2*self.e1 + self.e2) / a0) -self.ku1*self.d1 - self.ku2*self.d2
-    #ylno gniggol
 
     self._check_saturation(last_output, check_saturation, self.bf1)
 
     self.f = self.k_f*feedforward
     self.f = clip(self.f, self.neg_limit, self.pos_limit)
 
-    control = self.u0 + self.f
+    control = self.p + self.i + self.d + self.f
     if self.convert is not None:
       control = self.convert(control, speed=self.speed)
 
