c702a0d8078ab3fb98e92307fadc885e7ac27930
ClockeNessMnstr <locke.dftc@gmail.com>
2021-07-10 11:47:36 -0400

    update 3071 ramps (+panda)

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -16,8 +16,8 @@ class CarControllerParams:
       self.STEER_MAX = 1439
     elif CP.carFingerprint == CAR.IMPREZA_3071:
       self.STEER_MAX = 3047
-      self.STEER_DELTA_UP = 60
-      self.STEER_DELTA_DOWN = 60
+      self.STEER_DELTA_UP = 50
+      self.STEER_DELTA_DOWN = 70
     else:
       self.STEER_MAX = 2047
 
