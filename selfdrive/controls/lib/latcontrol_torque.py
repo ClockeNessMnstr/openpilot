6468a2b2b2608f6b215224e7e8e3272421f349ba
ClockeNessMnstr <locke.dftc@gmail.com>
2022-05-03 18:05:23 -0400

    discrete_torque

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -1,37 +1,18 @@
 import math
-from selfdrive.controls.lib.pid import PIDController
-from common.numpy_fast import interp
+from selfdrive.controls.lib.discrete import DiscreteController
+from common.numpy_fast import clip
 from selfdrive.controls.lib.latcontrol import LatControl, MIN_STEER_SPEED
-from selfdrive.controls.lib.vehicle_model import ACCELERATION_DUE_TO_GRAVITY
+from common.realtime import DT_CTRL
 from cereal import log
 
-# At higher speeds (25+mph) we can assume:
-# Lateral acceleration achieved by a specific car correlates to
-# torque applied to the steering rack. It does not correlate to
-# wheel slip, or to speed.
-
-# This controller applies torque to achieve desired lateral
-# accelerations. To compensate for the low speed effects we
-# use a LOW_SPEED_FACTOR in the error. Additionally there is
-# friction in the steering wheel that needs to be overcome to
-# move it at all, this is compensated for too.
-
-
 LOW_SPEED_FACTOR = 200
-JERK_THRESHOLD = 0.2
-
 
 class LatControlTorque(LatControl):
   def __init__(self, CP, CI):
     super().__init__(CP, CI)
-    self.pid = PIDController(CP.lateralTuning.torque.kp, CP.lateralTuning.torque.ki,
-                            k_f=CP.lateralTuning.torque.kf, pos_limit=1.0, neg_limit=-1.0)
-    self.get_steer_feedforward = CI.get_steer_feedforward_function()
+    self.pid = DiscreteController([CP.lateralTuning.torque.ki, CP.lateralTuning.torque.kp, CP.lateralTuning.torque.friction],  rate=(1 / DT_CTRL))
     self.steer_max = 1.0
-    self.pid.pos_limit = self.steer_max
-    self.pid.neg_limit = -self.steer_max
     self.use_steering_angle = CP.lateralTuning.torque.useSteeringAngle
-    self.friction = CP.lateralTuning.torque.friction
 
   def reset(self):
     super().reset()
@@ -43,37 +24,25 @@ class LatControlTorque(LatControl):
     if CS.vEgo < MIN_STEER_SPEED or not active:
       output_torque = 0.0
       pid_log.active = False
-      self.pid.reset()
+      self.reset()
     else:
       if self.use_steering_angle:
         actual_curvature = -VM.calc_curvature(math.radians(CS.steeringAngleDeg - params.angleOffsetDeg), CS.vEgo, params.roll)
       else:
         actual_curvature = llk.angularVelocityCalibrated.value[2] / CS.vEgo
-      desired_lateral_accel = desired_curvature * CS.vEgo**2
-      desired_lateral_jerk = desired_curvature_rate * CS.vEgo**2
-      actual_lateral_accel = actual_curvature * CS.vEgo**2
-
-      setpoint = desired_lateral_accel + LOW_SPEED_FACTOR * desired_curvature
-      measurement = actual_lateral_accel + LOW_SPEED_FACTOR * actual_curvature
-      error = setpoint - measurement
+        
+      error = -(desired_curvature - actual_curvature) *(CS.vEgo**2 + LOW_SPEED_FACTOR)
+      output_torque = self.pid.update(error, last_actuators.steer)
+      
+      output_torque = clip(output_torque, -self.steer_max, self.steer_max)
+      
       pid_log.error = error
-
-      ff = desired_lateral_accel - params.roll * ACCELERATION_DUE_TO_GRAVITY
-      output_torque = self.pid.update(error,
-                                      override=CS.steeringPressed, feedforward=ff,
-                                      speed=CS.vEgo,
-                                      freeze_integrator=CS.steeringRateLimited)
-
-      friction_compensation = interp(desired_lateral_jerk, [-JERK_THRESHOLD, JERK_THRESHOLD], [-self.friction, self.friction])
-      output_torque += friction_compensation
-
       pid_log.active = True
-      pid_log.p = self.pid.p
-      pid_log.i = self.pid.i
-      pid_log.d = self.pid.d
-      pid_log.f = self.pid.f
+      pid_log.i = float(self.pid.gains[0]*self.pid.d[0][1])
+      pid_log.p = float(self.pid.gains[1]*self.pid.d[1][1])
+      pid_log.d = float(self.pid.gains[2]*self.pid.d[2][1])
+      pid_log.f = 0
       pid_log.output = -output_torque
       pid_log.saturated = self._check_saturation(self.steer_max - abs(output_torque) < 1e-3, CS)
 
-    #TODO left is positive in this convention
-    return -output_torque, 0.0, pid_log
+    return output_torque, 0.0, pid_log
